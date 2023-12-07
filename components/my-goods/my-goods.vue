dbf1dda170a7a03e0bbc4fd8cb53162c2f3a9d78
Jane <1251756971@qq.com>
2023-12-07 17:44:46 +0800

    feat: 商品列表

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="goods-item">
+		<!-- 左边区域 -->
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo || defaultPic"></image>
+		</view>
+		<!-- 右边区域 -->
+		<view class="goods-item-right">
+			<view class="goods-item-name">{{goods.goods_name}}</view>
+			<view class="goods-item-price">￥{{goods.goods_price | toFixed}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props: {
+			goods: {
+				type: Object,
+				default: () => ({})
+			}
+		},
+		data() {
+			return {
+				defaultPic: 'http://image5.suning.cn/uimg/b2c/newcatentries/0000000000-000000000602669097_1_200x200.jpg'
+			};
+		},
+		filters: {
+			toFixed(num) {
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item {
+	display: flex;
+	padding: 10px 5px;
+	border-bottom: 1px solid #f0f0f0;
+	.goods-item-left {
+		margin-right: 5px;
+		image {
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+	}
+	.goods-item-right {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		
+		.goods-item-name {
+			font-size: 13px;
+		}
+		.goods-item-price {
+			font-size: 16px;
+			color: #C00000;
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		列表
+		<view @click="gotoDetail(item)" v-for="(item, index) in goodsList" :key="index">
+			<my-goods :goods="item"></my-goods>
+		</view>
 	</view>
 </template>
 
@@ -8,11 +10,69 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					// 关键字
+					query: '',
+					// 商品分类id
+					cid: '',
+					pageNum: 1,
+					pageSize: 10,
+				},
+				total: 0,
+				goodsList: [],
+				isLoading: false
 			}
 		},
-		methods: {
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			this.getGoodsList()
+		},
+		onReachBottom() {
+			// 判断是否还有下一页数据
+			if (this.queryObj.pageNum * this.queryObj.pageSize >= this.total) {
+				return uni.showToast({
+					title: "数据加载完毕",
+					duration: 1500,
+					icon: 'none'
+				})
+			}
+			// 节流，判断是否正在请求数据，如是，则不发起额外请求
+			if (this.isLoading) return
+			this.queryObj.pageNum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pageNum = 1
+			this.total = 0
+			this.goodsList = []
+			this.isLoading = false
 			
+			this.getGoodsList(() => uni.stopPullDownRefresh())
+		},
+		methods: {
+			async getGoodsList(cb) {
+				this.isLoading = true
+				const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isLoading = false
+				
+				cb && cb()
+				
+				if (res.meta.status !== 200) {
+					return uni.showToast({
+						title: "数据请求失败",
+						duration: 1500,
+						icon: 'none'
+					})
+				}
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoDetail(item) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
+			}
 		}
 	}
 </script>
